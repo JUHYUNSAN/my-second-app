16e4f7dda7a40e25c848958d1f93db9806e8141c
Juhyun Seo <tomato.202404.seojh@gmail.com>
2024-05-08 10:21:37 +0900

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipsesComponent } from './recipses/recipses.component';
+import { RecipeListComponent } from './recipses/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipses/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipses/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipsesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/recipses/recipses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipses',
+  templateUrl: './recipses.component.html',
+  styleUrl: './recipses.component.css'
+})
+export class RecipsesComponent {
+
+}
